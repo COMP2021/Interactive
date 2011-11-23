06e91132b897cb1f9f6319601d64e0173075e795
Guanlun ZHAO <guanlunzhao@gmail.com>
2011-11-23 19:28:54 +0800

    UI improved

--- painter/database.pl
+++ painter/database.pl
@@ -11,15 +11,15 @@ our @seg_db = ();
 # set the user's buffer to the newest line seg
 sub set_buffer {
   my ($user_id, $data) = @_;
-  $buffer{$user_id} = $data;
+  $buffers{$user_id} = $data;
 }
 
 # append a string to the buffer, only for drawing with a pen
 sub append_to_buffer {
   my ($user_id, $data) = @_;
   # use a delimeter separate the strings
-  if ($buffer{$user_id}) { # the buffer is defined
-    $buffer{$user_id} .= "|".$data;
+  if ($buffers{$user_id}) { # the buffer is defined
+    $buffers{$user_id} .= "|".$data;
   } else { # the buffer is empty
     set_buffer($user_id, $data);
   }
@@ -28,7 +28,7 @@ sub append_to_buffer {
 # push the data in the buffer into the database
 sub buffer2db {
   my $user_id = $_[0];
-  my $buf = $buffer{$user_id};
+  my $buf = $buffers{$user_id};
   if ($buf) {
     $buf =~ s/\"tentative\":1/\"tentative\":0/g;
   }
@@ -38,7 +38,7 @@ sub buffer2db {
 # discard the data in the buffer, used when the user click the mouse
 sub dump_buffer {
   my $user_id = $_[0];
-  $buffer{$user_id} = 0;
+  $buffers{$user_id} = 0;
 }
 
 # return the database
@@ -48,7 +48,7 @@ sub get_db {
 
 sub get_buffer {
   my $user_id = $_[0];
-  return $buffer{$user_id};
+  return $buffers{$user_id};
 }
 
 # return all the buffers
@@ -66,7 +66,7 @@ sub print_database {
 # print the info of a buffer, for debugging
 sub print_buffer {
   my $user_id = $_[0];
-  print "$buffer{$user_id}\n";
+  print "$buffers{$user_id}\n";
 }
 
 # print all, for debugging
